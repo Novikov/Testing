c2d8bbcfe94fb69c600fc9872db7aab486c480ee
Igor Novikov <novikov.mail@inbox.ru>
2021-04-17 19:38:35 +0300

    chapter 9 1/2

---
+++ Practice/9.Persistance/app/src/androidTest/java/com/raywenderlich/android/wishlist/persistence/persistence/WishlistDaoTest.kt
@@ -0,0 +1,33 @@
+package com.raywenderlich.android.wishlist.persistence.persistence
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.raywenderlich.android.wishlist.persistence.WishlistDao
+import com.raywenderlich.android.wishlist.persistence.WishlistDatabase
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WishlistDaoTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var wishlistDatabase: WishlistDatabase
+    private lateinit var wishlistDao: WishlistDao
+
+    @Before
+    fun initDb() {
+        wishlistDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().context, WishlistDatabase::class.java).build()
+        wishlistDao = wishlistDatabase.wishlistDao()
+    }
+
+    @After
+    fun closeDb() {
+        wishlistDatabase.close()
+    }
+}
